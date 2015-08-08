09245726cf3ea67d30b9e6f8302461d85d1ed7dd
yilo <successforevertim@gmail.com>
2015-08-08 18:54:46 +0800

    add sass and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+minifycss = require('gulp-minify-css'),
+jshint = require('gulp-jshint'),
+uglify = require('gulp-uglify'),
+imagemin = require('gulp-imagemin'),
+rename = require('gulp-rename'),
+clean = require('gulp-clean'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+cache = require('gulp-cache'),
+livereload = require('gulp-livereload');
+
+gulp.task("compile-foundation", function () {
+	return gulp.src('bower_components/foundation/scss/*.scss')
+	.pipe(sass({
+			includePaths : ['bower_components/foundation/scss']
+		}))
+	.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['compile-foundation']);
